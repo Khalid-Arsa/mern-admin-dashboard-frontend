281d0578275f9a94d102fde376bdc4f55cb161f9
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-04-25 16:33:38 +0800

    changing color

--- src/theme.js
+++ src/theme.js
@@ -28,6 +28,20 @@ export const tokensDark = {
     900: "#070812",
   },
   secondary: {
+    // light blue
+    0: "#ffffff", // manually adjusted
+    50: "#e1f5fe", // manually adjusted
+    100: "#b3e5fc",
+    200: "#81d4fa",
+    300: "#4fc3f7",
+    400: "#29b6f6",
+    500: "#03a9f4",
+    600: "#039be5",
+    700: "#0288d1",
+    800: "#0277bd",
+    900: "#01579b",
+  },
+  tertiary: {
     // yellow
     50: "#f0f0f0", // manually adjusted
     100: "#fff6e0",
@@ -39,7 +53,7 @@ export const tokensDark = {
     700: "#997d3d",
     800: "#665429",
     900: "#332a14",
-  },
+  }
 };
 
 // function that reverses the color palette
