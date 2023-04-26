57be264b2c49f8a3e215d44718a0fdd51bba12b3
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-04-26 11:01:30 +0800

    adjusting padding

--- src/scenes/transactions/index.jsx
+++ src/scenes/transactions/index.jsx
@@ -54,7 +54,7 @@ const Transactions = () => {
   ];
 
   return (
-    <Box m="1.5rem 2.5rem">
+    <Box m="1.5rem 2.5rem" paddingBottom={4}>
       <Header title="TRANSACTIONS" subtitle="Entire list of transactions" />
       <Box
         height="80vh"
